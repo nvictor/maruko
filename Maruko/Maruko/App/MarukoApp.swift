96251f8876811fb2cacbbde65aa49b9d2341f800
Victor Noagbodji <noagbodjivictor@gmail.com>
2026-04-23 18:41:27 -0400

    Add Sparkle updater and appcast release workflow

--- Maruko/Maruko/App/MarukoApp.swift
+++ Maruko/Maruko/App/MarukoApp.swift
@@ -3,10 +3,15 @@ import SwiftData
 
 @main
 struct MarukoApp: App {
+    @StateObject private var updater = AppUpdater()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
         .modelContainer(AppModelContainer.shared)
+        .commands {
+            CheckForUpdatesCommands(updater: updater)
+        }
     }
 }
